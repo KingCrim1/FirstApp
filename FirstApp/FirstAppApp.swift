8a4458654cccb8d6ab70125b2082b96cbb843181
Henk Dragt <hw.dragt@gmail.com>
2021-11-07 20:46:30 +0100

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by H W Dragt on 07/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
