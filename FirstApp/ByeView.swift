56409e9aada99e29493a972b89efa56ba65d51e7
Henk Dragt <hw.dragt@gmail.com>
2021-11-08 20:30:10 +0100

    Initieel

---
+++ FirstApp/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  FirstApp
+//
+//  Created by H W Dragt on 08/11/2021.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye Bye, World!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
